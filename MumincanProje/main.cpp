895cd417827682daedfe6880603dd5d2dc3375cb
MCucak <33839927+MCucak@users.noreply.github.com>
2020-05-09 04:40:23 +0300

    WorkInNow

--- MumincanProje/main.cpp
+++ MumincanProje/main.cpp
@@ -6,10 +6,10 @@
 #include<stdlib.h>
 #include <stdio.h>
 #include <time.h>       
+#include <random>
 
 using namespace std;
 #pragma endregion
-
 #pragma region Method Definitions
 void key_entry(int key, int x, int y);
 void display();
@@ -17,53 +17,54 @@ void plane();
 void setLocation();
 void helicopterStraight(int x, int y);
 void helicopterReverse(int x, int y);
-void gameManagement();
-
+void helikopter1();
+void helikopter2();
+void helikopter3();
+void parametreAl(int heliId);
+void isCrashed();
+std::default_random_engine generator;
+std::uniform_int_distribution<int> hizBulma(1, 6);
+std::uniform_int_distribution<int> YonBulma(0, 1);
 #pragma endregion
-
 #pragma region Variables
 int planeX, planeY;
-int windowSizeWidht = 480;
+int windowSizeWidth = 480;
 int windowSizeHeight = 640;
 int gameSpeed = 10;
-float helikopter1X=300, helikopter1Y=400, helikopter2X=200, helikopter2Y=400;
-
+float x1 = 0, x2 = 0, x3 = 0;
+int sagaHareket1 = YonBulma(generator);;
+int sagaHareket2 = YonBulma(generator);;
+int sagaHareket3= YonBulma(generator);;
+int difficulty = 1;
+float Speed1, Speed2, Speed3;
+int reset1 = 1, reset2 = 1, reset3 = 1;
+int hak = 3;
+int hx1, hx2, hx3, hy1, hy2, hy3;
 #pragma endregion
-
-void gameManagement() {
-	
-
-
-}
-
-void display() {
-	glClear(GL_COLOR_BUFFER_BIT);
-	plane();
-	gameManagement();
-	helicopterStraight(helikopter1X, helikopter1Y);
-	helicopterReverse(helikopter2X, helikopter2Y);
-	helikopter1X += 0.04;	
-	glutPostRedisplay();
-	glutSwapBuffers();
-}
-
+/*
 class Heli {
 public:
-	int Id;
-	int Yon;
-	int Hýz;
-	int Renk;
+	int x,y,isDirectionRight,extra;
+	float Speed;
 
-	Heli() {
-
-
+	Heli(int layer, int difficulty) {
+		srand(time(NULL));
+		isDirectionRight = rand() % 2;
+		if (isDirectionRight == 0) { x = 25; }
+		else { x = windowSizeWidth - 25; }
+		if (layer == 1) { y = 300; }
+		if (layer == 2) { y = 400; }
+		if (layer == 3) { y = 500; }
+		Speed = rand() % 7 + 1;
+		Speed = Speed * 0, 1 + difficulty;
+		helicopterUret();
 	}
-	void helicopterUret(int x, int y, int yon) {
+	void helicopterUret() {
 		int direction = 1;
 		glColor3f(0, 0, 1);
 		glPointSize(2.0f);
 		glLineWidth(2.0f);
-		if (yon == 1) {
+		if (isDirectionRight == 1) {
 #pragma region gövde
 			glBegin(GL_POLYGON); //1
 			glVertex2f((x - 5) * direction, y + 10);
@@ -122,7 +123,7 @@ public:
 			glEnd();
 #pragma endregion
 		}
-		if (yon == -1) {
+		if (isDirectionRight == 0) {
 #pragma region gövde
 			glBegin(GL_POLYGON); //1
 			glVertex2f(((x - 5) * direction) + 2 * x, y + 10);
@@ -183,11 +184,20 @@ public:
 		}
 		glFlush();
 	}
-
-	
 };
+*/
 
 
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	plane(); 
+	helikopter1();
+	helikopter2();
+	helikopter3();
+	isCrashed();
+	glutPostRedisplay();
+	glutSwapBuffers();
+}
 
 void setLocation() {
 	srand(time(NULL));
@@ -221,7 +231,7 @@ void key_entry(int key, int x, int y) {
 		break;
 	case GLUT_KEY_RIGHT:
 		planeX = planeX + gameSpeed;
-		if (planeX + 30 > windowSizeWidht) { planeX -= gameSpeed; }
+		if (planeX + 30 > windowSizeWidth) { planeX -= gameSpeed; }
 		printf("GLUT_KEY_RIGHT %d \n", planeX);
 		glutPostRedisplay();
 		break;
@@ -236,11 +246,10 @@ void key_entry(int key, int x, int y) {
 int main(int argc, char** argv)
 {
 	setLocation();
-
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(600, 300);//glut penceresinin ekran konumu
-	glutInitWindowSize(windowSizeWidht, windowSizeHeight); //glut penceresinin boyutlarý
+	glutInitWindowSize(windowSizeWidth, windowSizeHeight); //glut penceresinin boyutlarý
 	glutCreateWindow("Dinamikleþtik Aðam");
 	glClearColor(1, 1, 1, 0);
 	gluOrtho2D(0, 480, 0.0, 640);
@@ -337,6 +346,7 @@ void helicopterStraight(int x, int y) {
 #pragma endregion
 	glFlush();
 }
+
 void helicopterReverse(int x, int y) {
 	int direction = -1;
 	glColor3f(0, 0, 1);
@@ -400,6 +410,98 @@ void helicopterReverse(int x, int y) {
 	glEnd();
 #pragma endregion
 	glFlush();
+}
 
+void helikopter1(){
+	if(reset1==1){
+		parametreAl(1); 
+	}
+	if (sagaHareket1 == 1) {
+		helicopterStraight(25+x1, 300);
+		x1 += Speed1;
+		if (25 + x1 > 480) {
+			parametreAl(1);
+			x1 = 0;
+		}
+	}
+	if (sagaHareket1 == 0) {
+		helicopterReverse(480 - x1, 300);
+		x1 += Speed1;
+		if (480-x1 < 0) {
+			parametreAl(1);
+			x1 = 0;
+		}
+	}
+	hy1 = 300;
+}
+void helikopter2(){
+	if (reset2 == 1) {
+		parametreAl(2);
+	}
+	if (sagaHareket2 == 1) {
+		helicopterStraight(25+x2, 400);
+		x2 += Speed2;
+		if (25 + x2 > 480) {
+			parametreAl(2);
+			x2 = 0;
+		}
+	}
+	if (sagaHareket2 == 0) {
+		helicopterReverse(480 - x2, 400);
+		x2 += Speed2;
+		if (480 - x2 < 0) {
+			parametreAl(2);
+			x2 = 0;
+		}
+	}
+}
+void helikopter3(){
+
+	if (reset3 == 1) {
+		parametreAl(3);
+	}
+	if (sagaHareket3 == 1) {
+		helicopterStraight(25 + x3, 500);
+		x3 += Speed3;
+		if (25 + x3 > 480) {
+			parametreAl(3);
+			x3 = 0;
+		}
+	}
+	if (sagaHareket3 == 0) {
+		helicopterReverse(480 - x3, 500);
+		x3 += Speed3;
+		if (480 - x3 < 0) {
+			parametreAl(3);
+			x3 = 0;
+		}
+	}
+}
+void parametreAl(int heliId) {
+	if (heliId == 1) {
+		srand(time(NULL));
+		sagaHareket1= rand() % 2;
+		if(sagaHareket2 == 1){}
+		Speed1 = hizBulma(generator);
+		Speed1 = Speed1 * 0.01 + difficulty * 0, 2;
+		reset1 = 0;
+	}
+	if (heliId == 2) {
+		srand(time(NULL));
+		sagaHareket2 = rand() % 2;
+		Speed2 = hizBulma(generator);
+		Speed2 = Speed2 * 0.01 + difficulty * 0, 2;
+		reset2 = 0;
+	}
+	if (heliId == 3) {
+		srand(time(NULL));
+		sagaHareket1 = rand() % 2;
+		Speed3 = hizBulma(generator);
+		Speed3 = Speed3 * 0.01 + difficulty * 0, 2;
+		reset3 = 0;
+	}
+}
+
+void isCrashed() {
 
 }
